3fcd46ee61b4314e1adda38de8242f7776f3ff8c
mjsgreen01 <mjsgreen01@gmail.com>
2014-05-19 12:44:13 -0400

    First Commit

---
+++ homePage.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+     $("#940, li").hover(
+     	function(){
+         	$("#940").attr('src','homeImg2.jpg');
+     	},
+     	function(){
+         	$("#940").attr('src','homeImg1.jpg');
+     	}
+     );
+});
