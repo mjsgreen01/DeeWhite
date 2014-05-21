59706fb7557325f4421725a8419e51afc43d26f2
mjsgreen01 <mjsgreen01@gmail.com>
2014-05-21 17:11:21 -0400

    Home Page Animation added

--- homePage.js
+++ homePage.js
@@ -9,8 +9,16 @@ $(document).ready(function(){
      );
 
 
+	$("#homeImg").delay(0).animate({"opacity": "1"}, 2000);
 
+	
+	var easeNav = function(element, timeDelay){
+		$(element).delay(timeDelay).animate({"margin-left": "0px"}, 700);
+	}
 
-
-
+	easeNav("#homeli", 1300);
+	easeNav("#musicli", 1450);
+	easeNav("#bioli", 1600);
+	easeNav("#tourli", 1750);
+	easeNav("#contactli", 1900);
 });
